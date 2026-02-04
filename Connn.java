a6209b317d5e25d246c9819ead7674346158412b
Samiksha Jadhav <samikshajadhav354@gmail.com>
2026-02-04 11:47:04 +0530

    Connn.java

---
+++ Connn.java
@@ -0,0 +1,22 @@
+package bank.management.system;
+import java.sql.*;
+
+public class Connn {
+
+    Connection connection;
+    Statement statement;
+
+
+    public Connn(){
+
+        try{
+
+            Class.forName("com.mysql.cj.jdbc.Driver"); ///chat gptline
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","Asthetic@123");
+            statement =connection.createStatement();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
